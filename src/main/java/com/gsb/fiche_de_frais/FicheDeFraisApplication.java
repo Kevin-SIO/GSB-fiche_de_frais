8fc32291bf89a89f9f73130630d003e627f24815
Kevin Carbeti <kevincarbeti@gmail.com>
2022-10-13 15:10:31 +0200

    First commit

---
+++ src/main/java/com/gsb/fiche_de_frais/FicheDeFraisApplication.java
@@ -0,0 +1,13 @@
+package com.gsb.fiche_de_frais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FicheDeFraisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FicheDeFraisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gsb/fiche_de_frais/FicheDeFraisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gsb.fiche_de_frais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FicheDeFraisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
