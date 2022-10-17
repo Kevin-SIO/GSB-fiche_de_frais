f112acad9166ac3e52b4ed7d8731acfe9884ebf4
Kevin Carbeti <kevincarbeti@gmail.com>
2022-10-17 13:48:59 +0200

    rename folders

---
+++ src/main/java/com/gsb/fiche_de_frais/models/Role.java
@@ -0,0 +1,25 @@
+package com.gsb.fiche_de_frais.models;
+
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.List;
+
+@Data
+@Entity
+public class Role {
+
+    @Id
+    @Column(name="id_role")
+    private int id;
+
+    @Column(name = "libelle_role")
+    private String nom;
+
+    @OneToMany(mappedBy = "role")
+    private List<Utilisateur> utilisateurs;
+
+}
---
+++ src/main/java/com/gsb/fiche_de_frais/models/Utilisateur.java
@@ -0,0 +1,29 @@
+package com.gsb.fiche_de_frais.models;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.sql.Date;
+
+@Data
+@Entity
+public class Utilisateur {
+
+    @Id
+    @Column(name = "id_utilisateur")
+    private int id;
+
+    private String nom;
+    private String prenom;
+    private String login;
+    private String mdp;
+    private String adresse;
+    private String cp;
+    private String ville;
+    @Column(name = "date_embauche")
+    private Date dateEmbauche;
+    @ManyToOne
+    @JoinColumn(name = "id_role")
+    private Role role;
+
+}
